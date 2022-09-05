1d1726337ba403b377aedd9ad4773977401b5c57
hasanz71 <amihasan471@gmail.com>
2022-09-05 22:06:39 +0600

    gfgfgf

--- index.js
+++ index.js
@@ -8,28 +8,14 @@ function updateClock() {
   const minute = currentTime.getMinutes();
   const second = currentTime.getSeconds();
 
-  const hourDeg = (hour / 12) * 360;
-  const minuteDeg = (minute / 60) * 360;
   const secondDeg = (second / 60) * 360;
+  const minuteDeg = (secondDeg + minute / 60) * 360;
+  const hourDeg = (minuteDeg + hour / 12) * 360;
 
   hourEl.style.transform = `rotate(${hourDeg}deg)`;
   minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
   secondEl.style.transform = `rotate(${secondDeg}deg)`;
-
-  setInterval(() => {
-    const currentTime = new Date();
-    const hour = currentTime.getHours();
-    const minute = currentTime.getMinutes();
-    const second = currentTime.getSeconds();
-
-    const hourDeg = (hour / 12) * 360;
-    const minuteDeg = (minute / 60) * 360;
-    const secondDeg = (second / 60) * 360;
-
-    hourEl.style.transform = `rotate(${hourDeg}deg)`;
-    minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
-    secondEl.style.transform = `rotate(${secondDeg}deg)`;
-  }, 1000);
 }
 
 updateClock();
+setInterval(() => updateClock(), 1000);
