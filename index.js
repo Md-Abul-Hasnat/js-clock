e71621534c1b09e8dd16a3753b7e2a7642968819
hasanz71 <amihasan471@gmail.com>
2022-09-05 06:21:37 +0600

    dss

--- index.js
+++ index.js
@@ -16,22 +16,20 @@ function updateClock() {
   minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
   secondEl.style.transform = `rotate(${secondDeg}deg)`;
 
-  setInterval(updateClock, 1000);
+  setInterval(() => {
+    const currentTime = new Date();
+    const hour = currentTime.getHours();
+    const minute = currentTime.getMinutes();
+    const second = currentTime.getSeconds();
+
+    const hourDeg = (hour / 12) * 360;
+    const minuteDeg = (minute / 60) * 360;
+    const secondDeg = (second / 60) * 360;
+
+    hourEl.style.transform = `rotate(${hourDeg}deg)`;
+    minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
+    secondEl.style.transform = `rotate(${secondDeg}deg)`;
+  }, 1000);
 }
 
 updateClock();
-
-// setInterval(() => {
-//   const currentTime = new Date();
-//   const hour = currentTime.getHours();
-//   const minute = currentTime.getMinutes();
-//   const second = currentTime.getSeconds();
-
-//   const hourDeg = (hour / 12) * 360;
-//   const minuteDeg = (minute / 60) * 360;
-//   const secondDeg = (second / 60) * 360;
-
-//   hourEl.style.transform = `rotate(${hourDeg}deg)`;
-//   minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
-//   secondEl.style.transform = `rotate(${secondDeg}deg)`;
-// }, 1000);
